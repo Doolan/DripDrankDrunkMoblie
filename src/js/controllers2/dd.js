6aa917ed27f51cc51a3f74d6f232e8db0ca36fef
Steven Doolan <doolansr@rose-hulman.edu>
2017-01-22 05:19:31 -0500

    update dd

--- src/js/controllers2/dd.js
+++ src/js/controllers2/dd.js
@@ -8,51 +8,51 @@
  * Controller of the dripDrankDrunkMoblieApp
  */
 angular.module('dripDrankDrunkMoblieApp')
-  .controller('DdCtrl', ['$scope', '$state', function ($scope, $state) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('DdCtrl', ['$scope', '$state', 'DDService', function ($scope, $state, DDService) {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
 
-    var ddPageSetup = function () {
-      $('.ui.form')
-        .form({
-          fields: {
-            name: {
-              identifier: 'name',
-              rules: [
-                {
-                  type: 'empty',
-                  prompt: 'Please enter your Wingman&apos;s name'
-                }
-              ]
-            },
-            pnum: {
-              identifier: 'pnum',
-              rules: [
-                {
-                  type: 'integer[1000000000..9999999999]',
-                  prompt: 'Please enter your Wingman&apos;s phone number as a 10 digit number'
-                }
-              ]
-            }
-          },
-          inline: true,
-          onSuccess: function (event, fields) {
-            $state.go('user.drink');
-          }
+        var ddPageSetup = function () {
+            $('.ui.form')
+                .form({
+                    fields: {
+                        name: {
+                            identifier: 'name',
+                            rules: [
+                                {
+                                    type: 'empty',
+                                    prompt: 'Please enter your Wingman&apos;s name'
+                                }
+                            ]
+                        },
+                        pnum: {
+                            identifier: 'pnum',
+                            rules: [
+                                {
+                                    type: 'integer[1000000000..9999999999]',
+                                    prompt: 'Please enter your Wingman&apos;s phone number as a 10 digit number'
+                                }
+                            ]
+                        }
+                    },
+                    inline: true,
+                    onSuccess: function (event, fields) {
+                        DDService.setDD(fields.name, fields.pnum);
+                        $state.go('user.drink');
+                    }
+                });
+        };
+
+        $scope.$on('$viewContentLoaded', function () {
+            ddPageSetup();
         });
-    };
 
-    $scope.$on('$viewContentLoaded', function () {
-      ddPageSetup();
-    });
-
-    $scope.advance = function () {
-      $state.go('user.drink');
-    };
+        $scope.advance = function () {
+            $state.go('user.drink');
+        };
 
 
-
-  }]);
+    }]);
